c7d013c5f8a2bff944cdbe725d3efa2030707937
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 18:37:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  call = []
+  planeteer_calls.map do |element|
+    call << element.
 end
 
 def long_planeteer_calls# code an argument here
