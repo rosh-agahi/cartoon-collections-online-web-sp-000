c822db73a22d1b1adb317d94acd0081fdd8b5c86
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 18:51:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,5 @@ def find_the_cheese(recipe)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  shared recipe
-end
+  cheese = cheese_types & recipe
 end
